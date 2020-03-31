4d6b06358f15b57f4299e8de28d0b1b422a2773f
Kolby Kruger <kolbykruger@gmail.com>
2020-03-31 12:48:55 -0400

    added class and updated aria attributes on events

--- js/ada.js
+++ js/ada.js
@@ -2,25 +2,34 @@ document.addEventListener('DOMContentLoaded', function() {
     var adaBar = document.querySelector('.ada-accessibility'),
         adaButtons = adaBar.querySelectorAll('button');
 
-        for(let i = 0; i < adaButtons.length; i++) {
-            adaButtons[i].addEventListener('click', function() {
+    adaBar.addEventListener('focusin', function() {
+        adaBar.setAttribute('aria-expanded', true)
+        adaBar.classList.add('ada-accessibility-expanded');
+    })
+    adaBar.addEventListener('focusout', function() {
+        adaBar.setAttribute('aria-expanded', false)
+        adaBar.classList.remove('ada-accessibility-expanded');
+    })
 
-                if (this.id === 'ada-navigation') {
-                    var nav = document.querySelector('.nav');
+    for (let i = 0; i < adaButtons.length; i++) {
+        adaButtons[i].addEventListener('click', function() {
 
-                    //Detect mobile nav vs desktop nav
-                    if (window.getComputedStyle(nav).display === "none") {
-                        nav = document.querySelector('.navicon');
-                    }
-                    nav.tabIndex = '0';
-                    nav.focus();
+            if (this.id === 'ada-navigation') {
+                var nav = document.querySelector('.nav');
+
+                //Detect mobile nav vs desktop nav
+                if (window.getComputedStyle(nav).display === "none") {
+                    nav = document.querySelector('.navicon');
                 }
+                nav.tabIndex = '0';
+                nav.focus();
+            }
 
-                if (this.id === 'ada-content') {
-                    var content = document.querySelector('main section:first-of-type');
-                    content.tabIndex = '0';
-                    content.focus();
-                }
-            })
-        }
+            if (this.id === 'ada-content') {
+                var content = document.querySelector('main section:first-of-type');
+                content.tabIndex = '0';
+                content.focus();
+            }
+        })
+    }
 })
