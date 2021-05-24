aab1a0d17b23abb76a75a061e149ae84d4388919
Rob Liberty <rob@robliberty.com>
2021-05-24 09:15:00 -0400

    Blog category select
    Added a script to populate the Blog category select on page load correctly

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,25 @@ let blogCategories = {
     }
 }.init();
 
+//Populate Blog Category select 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+var blogCat = getUrlParameter('category');
+$('#blog_categories').val(blogCat);
+
 //Responsive iFrame
 $('iframe[src*="youtube"],iframe[src*="vimeo"]').wrap('<div class="responsive-iframe"/>');
 
