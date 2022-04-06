e3f9e6cbb3331adeb8af05c932faa3d7d47f73b3
Kolby Kruger <kolbykruger@gmail.com>
2022-04-06 11:04:57 -0400

    landing page created

--- js/demo.js
+++
@@ -1,4 +0,0 @@
-// Links return false
-$('a:not(.force), button:not(.force), input[type="submit"]:not(.force)').click(function() {
-    return false
-})
--- js/fonts.js
+++ js/fonts.js
@@ -1,18 +1,19 @@
-(function() {
+;(function () {
     WebFontConfig = {
-      active: function() {
-        sessionStorage.fonts = true;
-      },
-      google: {
-        families: ['Open+Sans:300,400,400i,500,600,700']
-      },
-      timeout: 2000
-    };
-
-    (function(d) {
-      var wf = d.createElement('script'), s = d.scripts[0];
-      wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-      wf.async = true;
-      s.parentNode.insertBefore(wf, s);
-    })(document);
-})();
+        active: function () {
+            sessionStorage.fonts = true
+        },
+        google: {
+            families: ['Roboto:300,400,400i,500,600,700'],
+        },
+        typekit: { id: 'rar4shm' },
+        timeout: 2000,
+    }
+    ;(function (d) {
+        var wf = d.createElement('script'),
+            s = d.scripts[0]
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
+        wf.async = true
+        s.parentNode.insertBefore(wf, s)
+    })(document)
+})()
